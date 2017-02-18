64ab0b4e830d546528450d528f7fdf4ce0ee8be8
preeti <preetijain387@gmail.com>
2017-02-18 16:29:46 +0530

    commit

--- src/main/java/com/global/login/dao/impl/LoginDaoImpl.java
+++ src/main/java/com/global/login/dao/impl/LoginDaoImpl.java
@@ -11,9 +11,7 @@ import org.springframework.util.StringUtils;
 import com.global.login.constants.QueryConstants;
 import com.global.login.dao.LoginDao;
 import com.global.login.entity.Login;
-import com.global.login.entity.Patient;
 import com.global.login.extractor.LoginExtractor;
-import com.global.login.extractor.PatientExtractor;
 import com.global.login.request.LoginRequest;
 @Repository
 public class LoginDaoImpl implements LoginDao{
